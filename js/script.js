99d50d206d329c2d2613a2f05c65241ba1df5923
Alexandre Dias <116679332+AleDiias@users.noreply.github.com>
2023-09-21 11:39:19 -0300

    adjust calc

--- js/script.js
+++ js/script.js
@@ -24,11 +24,11 @@ function interpretarResultado(imc) {
         return "Abaixo do peso";
     } else if (imc >= 18.5 && imc < 24.9) {
         return "Peso normal";
-    } else if (imc >= 25 && imc < 29.9) {
+    } else if (imc >= 24.9 && imc < 29.9) {
         return "Sobrepeso";
-    } else if (imc >= 30 && imc < 34.9) {
+    } else if (imc >= 29.9 && imc < 34.9) {
         return "Obesidade Grau I";
-    } else if (imc >= 35 && imc < 39.9) {
+    } else if (imc >= 34.9 && imc < 39.9) {
         return "Obesidade Grau II";
     } else {
         return "Obesidade Grau III";
